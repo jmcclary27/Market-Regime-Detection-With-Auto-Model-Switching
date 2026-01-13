954b8f39190a33a4e268b5cb0d90ce25f85a2556
Jaden <jadenmcclary@gmail.com>
2026-01-13 15:00:43 -0600

    CI fixes

--- src/features/__init__.py
+++
@@ -1,8 +0,0 @@
-from .builder import FeatureBuildConfig, build_features
-
-"""
-Feature pipeline v0.
-
-This package builds deterministic features from ingested market bars and writes
-artifacts to data/features/.
-"""
