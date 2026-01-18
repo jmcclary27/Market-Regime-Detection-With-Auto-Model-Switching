defcabbcc325b2516fbf4ab97efaee036f092864
Jaden <jadenmcclary@gmail.com>
2026-01-18 12:55:37 -0600

    airflow

--- airflow/dags/pipeline_one_off.py
+++ airflow/dags/pipeline_one_off.py
@@ -33,33 +33,37 @@ with DAG(
     # If you already have your own timestamp defaulting inside the module, you can omit passing run_ts.
     poll = BashOperator(
         task_id="poll",
-        bash_command=_cmd("src.poll.run", '--run-ts "{{ dag_run.conf.get(\'run_ts\', \'\') }}"'),
+        bash_command=_cmd("src.poll.run", "--run-ts \"{{ dag_run.conf.get('run_ts', '') }}\""),
     )
 
     features = BashOperator(
         task_id="features",
-        bash_command=_cmd("src.features.run", '--run-ts "{{ dag_run.conf.get(\'run_ts\', \'\') }}"'),
+        bash_command=_cmd("src.features.run", "--run-ts \"{{ dag_run.conf.get('run_ts', '') }}\""),
     )
 
     regimes = BashOperator(
         task_id="regimes",
-        bash_command=_cmd("src.regimes.run", '--run-ts "{{ dag_run.conf.get(\'run_ts\', \'\') }}"'),
+        bash_command=_cmd("src.regimes.run", "--run-ts \"{{ dag_run.conf.get('run_ts', '') }}\""),
     )
 
     predict = BashOperator(
         task_id="predict",
-        bash_command=_cmd("src.inference.run_batch_predict", '--run-ts "{{ dag_run.conf.get(\'run_ts\', \'\') }}"'),
+        bash_command=_cmd(
+            "src.inference.run_batch_predict", "--run-ts \"{{ dag_run.conf.get('run_ts', '') }}\""
+        ),
     )
 
     evaluate = BashOperator(
         task_id="evaluate",
-        bash_command=_cmd("src.eval.run_evaluator", '--run-ts "{{ dag_run.conf.get(\'run_ts\', \'\') }}"'),
+        bash_command=_cmd(
+            "src.eval.run_evaluator", "--run-ts \"{{ dag_run.conf.get('run_ts', '') }}\""
+        ),
         trigger_rule=TriggerRule.ALL_DONE,
     )
 
     switch = BashOperator(
         task_id="switch",
-        bash_command=_cmd("src.switcher.run", '--run-ts "{{ dag_run.conf.get(\'run_ts\', \'\') }}"'),
+        bash_command=_cmd("src.switcher.run", "--run-ts \"{{ dag_run.conf.get('run_ts', '') }}\""),
         trigger_rule=TriggerRule.ALL_DONE,
     )
 
